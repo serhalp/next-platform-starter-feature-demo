006d87f08b01c347a6c811e1f61500348e254300
Philippe Serhal <philippe.serhal@netlify.com>
2024-05-22 09:46:03 -0400

    add logging to middleware

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,5 +1,6 @@
 const rewrite = async (request, context) => {
     const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
+    console.log('rewrite middleware', { path, context, request });
     return new URL(path, request.url);
 };
 
